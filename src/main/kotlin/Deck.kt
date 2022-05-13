fec85fb26aa3c25158e10a5368d8a5eaf972234f
Reevak05 <47231296+reevak05@users.noreply.github.com>
2022-05-13 10:37:23 -0700

    Create part of basic structure

---
+++ src/main/kotlin/Deck.kt
@@ -0,0 +1,5 @@
+class Deck {
+    val deck = mutableListOf<Card>()
+}
+class Card(val value: Int, val suit: String) {
+}
---
+++ src/main/kotlin/PokerClub.kt
@@ -0,0 +1,8 @@
+class PokerClub {
+    val players = mutableListOf<Player>()
+    val tableCards = mutableListOf<Card>()
+}
+
+fun main(args: Array<String>) {
+    println("Hello World!")
+}
