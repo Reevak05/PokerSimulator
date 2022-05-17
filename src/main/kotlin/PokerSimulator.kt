bcda5424f4bc29fd95c62a250c2c9b81e52cc3b1
Reevak05 <47231296+reevak05@users.noreply.github.com>
2022-05-17 12:36:50 -0700

    Add dealing

--- src/main/kotlin/PokerSimulator.kt
+++ src/main/kotlin/PokerSimulator.kt
@@ -1,14 +1,20 @@
-class PokerSimulator {
+class PokerSimulator(val playerCount: Int = 0, val startingAmount: Int = 0) {
     val players = mutableListOf<Player>()
     val deck = mutableListOf<Card>()
     val discardPile = mutableListOf<Card>()
     val tableCards = mutableListOf<Card>()
-    var playerCount = 0
-    var startingAmount = 0
+    var bigBlindIndex = 0
+    var smallBlindIndex = 0
 
-    fun playGame(playerCount: Int, startingAmount: Int) {
-        this.playerCount = playerCount
-        this.startingAmount = startingAmount
+    fun playGame() {
+
+        for (i in 0 until playerCount) players.add(Player(startingAmount))
+
+        initializeDeck()
+
+        dealCards()
+
+        cheatersView()
 
     }
 
@@ -21,10 +27,25 @@ class PokerSimulator {
         deck.shuffle()
     }
 
+    fun dealCards() {
+        for (i in bigBlindIndex until 2 * playerCount + bigBlindIndex) {
+            players[i % playerCount].hand.add(deck.removeFirst())
+        }
+    }
+
+    fun cheatersView() {
+        println("table cards: ")
+        println(tableCards)
+        for (player in players) {
+            println("\nplayer:")
+            println("hand: ${player.hand.joinToString(", ") { "${it.value} of ${it.suit}" }}")
+            println("balance: ${player.balance}")
+        }
+    }
 
 
 }
 
 fun main(args: Array<String>) {
-    PokerSimulator().playGame(5, 300)
+    PokerSimulator(5, 300).playGame()
 }
