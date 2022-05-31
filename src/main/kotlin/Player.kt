c1c2c70455961808278ce259aa09b7b1c87a4e70
Reevak05 <47231296+reevak05@users.noreply.github.com>
2022-05-31 01:01:00 -0700

    Add player betting

--- src/main/kotlin/Player.kt
+++ src/main/kotlin/Player.kt
@@ -1,3 +1,78 @@
-class Player(var balance: Int) {
+import java.util.Scanner
+
+class Player(var balance: Int, val name: String = "Example Player") {
     var hand = mutableListOf<Card>()
+    var folded = false
+    var currentBet = 0
+
+    // Prompts each player to play their turn
+    // Returns their action and the amount they add to the pot
+    fun play(currentBet: Int): Pair<Actions, Int> {
+        val betIncrease = currentBet - this.currentBet;
+        println("\n$name, play your turn:")
+        println("Balance: $balance")
+        println("Your cards: ${hand.joinToString(", ") { "${it.value} of ${it.suit}" }}")
+        println("Current bet: $currentBet")
+        println("Amount you have put in pot: ${this.currentBet}")
+        println("   Increase over previous bet: $betIncrease")
+            val input = Scanner(System.`in`)
+        if (betIncrease == 0) println("Enter your action (check, bet, fold):")
+        else println("Enter your action (call, raise, fold):")
+        val result = when (input.next().lowercase()) {
+            "check" -> {
+                if (betIncrease == 0) {
+                    Pair(Actions.CHECK, 0)
+                } else {
+                    println("Checking is not an option because the current bet is not 0. Please try again:")
+                    return play(betIncrease)
+                }
+            }
+            "bet" -> {
+                if (betIncrease == 0) {
+                    println("Please enter how much you would like to bet:")
+                    var bet = input.nextInt()
+                    if (bet > balance) {
+                        println("That is more money than you have, so you have gone all in. Good luck!")
+                        bet = balance
+                    }
+                    Pair(Actions.BET, bet)
+                } else {
+                    println("Betting is not an option because the current bet is not 0. Use \"raise\" to increase the current bet. Please try again:")
+                    return play(betIncrease)
+                }
+            }
+            "call" -> {
+                if (betIncrease == 0) {
+                    println("The current bet is 0, so calling it is a check.")
+                    Pair(Actions.CHECK, 0)
+                } else {
+                    Pair(Actions.CALL, betIncrease)
+                }
+            }
+            "fold" -> {
+                folded = true
+                Pair(Actions.FOLD, 0)
+            }
+            "raise" -> {
+                println("Please enter by how much you would like to raise the bet:")
+                var bet = input.nextInt() + betIncrease
+                if (bet > balance) {
+                    println("That is more money than you have, so you have gone all in. Good luck!")
+                    bet = balance
+                }
+                Pair(Actions.RAISE, bet)
+            }
+            else -> {
+                println("Invalid choice. Please try again:")
+                play(betIncrease)
+            }
+        }
+        this.currentBet += result.second
+        this.balance -= result.second
+        return result;
+    }
+}
+
+enum class Actions {
+    CHECK, BET, FOLD, CALL, RAISE
 }
--- src/main/kotlin/PokerSimulator.kt
+++ src/main/kotlin/PokerSimulator.kt
@@ -1,22 +1,54 @@
-class PokerSimulator(val playerCount: Int = 0, val startingAmount: Int = 0) {
+import java.sql.SQLOutput
+
+class PokerSimulator(val playerNames: List<String>, val startingAmount: Int = 0) {
+    val playerCount: Int = playerNames.size
     val players = mutableListOf<Player>()
     val deck = mutableListOf<Card>()
     val discardPile = mutableListOf<Card>()
     val tableCards = mutableListOf<Card>()
-    var bigBlindIndex = 0
+    var pot = 0
+    var bigBlindIndex = 1
     var smallBlindIndex = 0
+    val bigBlind = 5
+    val smallBlind = 2
+    var currentBet = 0
 
     fun playGame() {
 
-        for (i in 0 until playerCount) players.add(Player(startingAmount))
+        // Add players
+        for (name in playerNames) players.add(Player(startingAmount, name))
 
+        // Initialize deck
         initializeDeck()
 
+        // Deal two cards to each player
         dealCards()
 
         cheatersView()
 
+        // Add blinds to pot
+        players[smallBlindIndex].addMoneyToPot(smallBlind)
+        players[bigBlindIndex].addMoneyToPot(bigBlind)
+
+        currentBet = bigBlind
+
+        bettingRound()
+
+        revealCards()
+
+        bettingRound()
+
+        revealCards()
+
+        bettingRound()
+
+        revealCards()
+
+        bettingRound()
+
+
     }
+
 
     fun initializeDeck() {
         for (suit in listOf("spades", "hearts", "diamonds", "clubs")) {
@@ -36,16 +68,92 @@ class PokerSimulator(val playerCount: Int = 0, val startingAmount: Int = 0) {
     fun cheatersView() {
         println("table cards: ")
         println(tableCards)
+        println("pot: $pot")
         for (player in players) {
-            println("\nplayer:")
+            println("\nplayer: ${player.name}")
             println("hand: ${player.hand.joinToString(", ") { "${it.value} of ${it.suit}" }}")
             println("balance: ${player.balance}")
         }
     }
 
+    fun revealCards() {
+        discardPile.add(deck.removeFirst())
+        for (i in 0 until when (tableCards.size) {
+            0 -> 3
+            3 -> 1
+            4 -> 1
+            else -> 0
+        }) {
+            tableCards.add(deck.removeFirst())
+        }
+        println("\nANNOUNCER: I will now reveal some cards on the table.")
+        println("ANNOUNCER: The table cards are: ${tableCards.joinToString(", ") { "${it.value} of ${it.suit}" }}")
+    }
+
+    fun Player.addMoneyToPot(amount: Int) {
+        this.balance -= amount
+        this.currentBet += amount
+        pot += amount
+    }
+
+    // Present players with the opportunity to bet: go around the table at least once, then continue as long as the bets are not settled
+    fun bettingRound() {
+        var i = bigBlindIndex + 1
+        val oneRoundIndex = bigBlindIndex + playerCount
+        while (!betsSettled() || i <= oneRoundIndex) {
+            if (!players[i % playerCount].folded) {
+                val playResult = players[i % playerCount].play(currentBet)
+                println("ANNOUNCER: ${players[i % playerCount].name} has chosen to ${playResult.first.name}, adding ${playResult.second} to the pot.")
+                if (playResult.first == Actions.RAISE || playResult.first == Actions.BET) currentBet = players[i % playerCount].currentBet
+                pot += playResult.second
+            }
+            i++
+        }
+
+    }
+
+    fun betsSettled(): Boolean {
+        for (player in players) {
+            if (!player.folded && player.currentBet < currentBet) return false
+        }
+        return true
+    }
+
+    fun distributeWinnings() {
+
+    }
+
+    fun calculateHandValue(playerHand: MutableList<Card>, tableCards: MutableList<Card>): Double {
+        val cards: List<Card> = playerHand + tableCards
+        return 0.0
+    }
+
+
+    // hand identification
+    fun containsPair(cards: List<Card>): Boolean {
+        for (i in cards.indices) {
+            for (j in i + 1 until cards.size) {
+                if (cards[i].value == cards[j].value) return true
+            }
+        }
+        return false
+    }
+
+    fun containsTwoPair(cards: List<Card>): Boolean {
+        var pairs = 0
+        for (i in cards.indices) {
+            for (j in i + 1 until cards.size) {
+                if (cards[i].value == cards[j].value) {
+                    pairs++
+                    if (pairs == 2) return true
+                }
+            }
+        }
+        return false
+    }
 
 }
 
 fun main(args: Array<String>) {
-    PokerSimulator(5, 300).playGame()
+    PokerSimulator(listOf("Player 1", "Player 2", "Player 3", "Player 4", "Player 5"), 300).playGame()
 }
